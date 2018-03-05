11bd87c96f55fea3165d44eb148606fcb2e9c074
Shendul <xiransonja@gmail.com>
2018-03-05 10:55:13 -0700

    Added base game concept

---
+++ Assets/Scripts/CellShapeSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellShapeSpawner : MonoBehaviour {
+
+
+	public GameObject[] cellShapes;
+	public GameObject[] nextCellShapes;
+
+	GameObject upNextObject = null;
+
+	int shapeIndex = 0;
+	int nextShapeIndex = 0;
+
+	public void SpawnShape(){
+
+		shapeIndex = nextShapeIndex;
+
+		Instantiate (cellShapes [shapeIndex],
+			transform.position,
+			Quaternion.identity);
+
+		nextShapeIndex = Random.Range (0, 8);
+
+		Vector3 nextShapePos = new Vector3 (-11, 11, 0);
+
+		if (upNextObject != null)
+			Destroy (upNextObject);
+
+		upNextObject = Instantiate (nextCellShapes [nextShapeIndex], 
+			nextShapePos,
+			Quaternion.identity);
+	}
+
+	void Start () {
+
+		nextShapeIndex = Random.Range (0, 8);
+		SpawnShape();
+
+	}
+	
+}
---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameBoard : MonoBehaviour {
+
+	public static Transform[,] gameBoard = new Transform[15, 30];
+
+	public static bool DeleteAllFullRows() {
+		// DeleteAllFullRows is meant to delete a row if every cell
+		// is full. Award the player for each symbol combined.
+
+		for (int row = 0; row < 30; ++row) {
+
+			if (IsRowFull (row)) {
+
+				DeleteGBRow (row);
+
+				// Temp sound, need to find better SFX.
+				SoundManager.Instance.PlayOneShot (SoundManager.Instance.rowDelete);
+
+				return true;
+			}
+				
+		}
+		return false;
+
+	}
+
+	public static bool IsRowFull(int row) {
+		// IsRowFull is meant to return true if a row is full of cells.
+
+		for (int col = 0; col < 15; ++col) {
+
+			if (gameBoard [col, row] == null) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void DeleteGBRow(int row) {
+		// DeleteGBRow removes all cells in a row on the gameboard.
+
+		for (int col = 0; col < 15; ++col) {
+
+			Destroy (gameBoard [col, row].gameObject);
+
+			gameBoard [col, row] = null;
+		}
+
+		row++;
+
+		for (int j = row; j < 30; ++j) {
+
+			for (int col = 0; col < 15; ++col) {
+
+				if (gameBoard [col, j] != null) {
+
+					gameBoard [col, j - 1] = gameBoard [col, j];
+					gameBoard [col, j] = null;
+					gameBoard[col, j-1].position += new Vector3(0, -1, 0);
+
+				}
+			}
+		}
+	}
+
+}
